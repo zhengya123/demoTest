a528d10d7322ae6c1763ac5418a3cd64fb24fbcd
zhengya <zhengya123>
2018-03-08 17:50:15 +0800

    Initial Commit

---
+++ demoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoTest
+//
+//  Created by 中商国际 on 2018/3/8.
+//  Copyright © 2018年 中商国际. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
